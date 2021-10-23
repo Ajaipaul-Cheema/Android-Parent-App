745ce8091f9ef771069779ac5f28666a7f3ddec6
aca242 <aca242@sfu.ca>
2021-10-23 13:51:38 -0700

    Initial Commit

---
+++ app/src/main/java/ca/cmpt276/as3/parentapp/UI/MainActivity.java
@@ -0,0 +1,33 @@
+package ca.cmpt276.as3.parentapp.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import ca.cmpt276.as3.parentapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+    }
+
+}
