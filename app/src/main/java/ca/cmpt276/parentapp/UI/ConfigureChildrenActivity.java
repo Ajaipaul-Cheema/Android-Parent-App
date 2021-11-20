421e647ec0a05c1a5c37b0dd544b7265cbba4deb
aca242 <aca242@sfu.ca>
2021-11-19 20:02:07 -0800

    Optimize imports and extract string resource

--- app/src/main/java/ca/cmpt276/parentapp/UI/ConfigureChildrenActivity.java
+++ app/src/main/java/ca/cmpt276/parentapp/UI/ConfigureChildrenActivity.java
@@ -16,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
